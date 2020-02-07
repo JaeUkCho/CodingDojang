250699549f2ea6a9cbdc0e87fbed7038da13d04a
Jaeuk Cho <chicuk9001@gmail.com>
2020-02-07 10:13:11 +0900

    CREATE OilerProject

---
+++ OilerProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OilerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = CalculatorProcess();
+
+            Console.WriteLine(result);
+        }
+
+        static int CalculatorProcess()
+        {
+            int result = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
