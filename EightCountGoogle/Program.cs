4e0aea94617d79e418dcd07db5cda736f17c72e8
Jaeuk Cho <chicuk9001@gmail.com>
2020-02-07 10:39:34 +0900

    CREATE EightCountGoogle

---
+++ EightCountGoogle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EightCountGoogle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = CalculatorProcess();
+
+            Console.WriteLine(result);
+        }   
+
+        static int CalculatorProcess()
+        {
+            int count = 0;
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                int j = i;
+                while (j > 0)
+                {
+                    if (j % 10 == 8)
+                    {
+                        count++;
+                    }
+                    
+                    j /= 10;                    
+                }
+            }           
+
+            return count;
+        }             
+    }
+}
